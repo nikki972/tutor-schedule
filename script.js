59168c3c5475db6af1bc642fb3e29fa888aa0612
nikki972 <nik_vo2002@mail.ru>
2026-02-19 21:31:58 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Приложение запущено');
+  const menuBtn = document.getElementById('menuBtn');
+  const sideMenu = document.getElementById('sideMenu');
+  const overlay = document.getElementById('overlay');
+
+  menuBtn.addEventListener('click', () => {
+    sideMenu.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+
+  overlay.addEventListener('click', () => {
+    sideMenu.classList.add('hidden');
+    overlay.classList.add('hidden');
+  });
 });
